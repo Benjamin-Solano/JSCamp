e8262ff4cf98ae55a9b8e2b5ee7230d5b61f3ac6
Benjamín <benjaminalexander958@gmail.com>
2025-10-11 20:25:28 -0600

    Ejercicio 1.1 - 1.2
    Componentes de React

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Desarrollo de componentes en React utilizando props para pasar datos
+const Cabecera = (props) => {
+  return (
+    <h1><i>{props.curso}</i></h1>
+  )
+}
+
+const Parte = (props) => {
+  return (
+    <h2> {props.parte} = {props.ejercicio}</h2>
+  )
+}
+
+const Contenido = (props) => {
+  return (
+    <>
+      <Parte parte={props.parte} ejercicio={props.ejercicio} />
+    </>
+  )
+}
+
+const totalEjercicios = (ex1, ex2, ex3) => {
+  return (
+    <div>
+      <h3>Total de ejercicios: {ex1 + ex2 + ex3}</h3>
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Definición de variables para el curso y las partes
+  const curso = "Half Stack application development"
+  const parte1 = "Fundamentos de React"
+  const ejercicio1 = 10
+  const parte2 = "Usando props para pasar datos"
+  const ejercicio2 = 7
+  const parte3 = "Estado de un componente"
+  const ejercicio3 = 14
 
+
+  // aquí se pasa la prop curso al componente Cabecera,
+  // aquí se pasan las props parte y ejercicio al componente Contenido, y
+  // aquí se llama a la función totalEjercicios para mostrar el total de ejercicios.
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Cabecera curso={curso} />
+
+        <Contenido parte={parte1} ejercicio={ejercicio1} />
+        <Contenido parte={parte2} ejercicio={ejercicio2} />
+        <Contenido parte={parte3} ejercicio={ejercicio3} />
+
+        {totalEjercicios(ejercicio1, ejercicio2, ejercicio3)}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
